77adcb5c218b70262c04e122afb74d633045609a
Pete Inge <pete.inge@gmail.com>
2014-12-17 22:55:35 -0500

    - spell manager to form
    Signed-off-by: Pete Inge <pete.inge@gmail.com>

--- sites/all/modules/custom/dd5_spell_manager/js/spell-manager.js
+++ sites/all/modules/custom/dd5_spell_manager/js/spell-manager.js
@@ -5,22 +5,24 @@ Drupal.behaviors.spellManager = {
 
     // Toggle whether a spell is prepared.
     $('input.spell-prepared-toggle').click(function() {
+/*
       var path = '/dd5-ajax/spell-manager/spell-prepared/' + $(this).data('char-id') + '/' + $(this).data('spell-id') + '/' + $(this).attr('checked');
 console.log(path);
       $.post(path, function( data ) {
 console.log( data );
       });
-
+*/
     });
 
     //
     $('input.spell-cast-toggle').click(function() {
+/*
       var path = '/dd5-ajax/spell-manager/spell-cast/' + $(this).data('char-id') + '/' + $(this).data('spell-id') + '/' + $(this).data('spell-slot-level') + '/' + $(this).data('spell-slot-level-delta') + '/' + $(this).attr('checked');
 console.log(path);
       $.post(path, function( data ) {
 console.log( data );
       });
-
+*/
     });
 
   }
