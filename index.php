1e4937eef1f509198ddd4d412ce6b5ff84ceacb9
Pete Inge <pete.inge@gmail.com>
2014-12-23 17:57:13 -0500

    - report all php errors
    Signed-off-by: Pete Inge <pete.inge@gmail.com>

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+// Report all PHP errors (see changelog)
+error_reporting(E_ALL);
+
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
 /**
  * @file
  * The PHP page that serves all page requests on a Drupal installation.
